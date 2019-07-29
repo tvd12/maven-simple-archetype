9d1ebaade87735c81d68aaf5defc90be044820c3
Dung Ta Van <itprono3@gmail.com>
2019-07-29 21:51:15 +0700

    release version 1.0.2

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,12 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+}
---
+++ src/main/resources/archetype-resources/src/test/java/testing/AppTest.java
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.testing;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assert Boolean.TRUE;
+	}
+	
+}
